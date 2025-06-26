3aa8ab128a9a2b212beda280b903da08250afa6b
Horizon <mgthedasher@gmail.com>
2025-06-26 12:44:24 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import 'bulma/css/bulma.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

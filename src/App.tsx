67af448fc710a873845d1d67e20b3b5fec0fa26f
Horizon <mgthedasher@gmail.com>
2025-06-26 12:43:55 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="section">
+      <div className="container">
+        <h1 className="title mb-4">Welcome to the WAC Site!</h1>
+        <p className="subtitle mb-5">This is the WAC site for Windows Animations!</p>
+      <h2 className="title p-4">What is WAC?</h2>
+      <p className="subtitle mb-4">WAC stands for Windows Animation Community. It is a community that people make stories, animations about Windows OSes.</p>
+      <h2 className="title mb-4">Series to watch!</h2>
+      <p className="subtitle mb-4">Here are some recommended series to watch! Definitely watch them :)</p>
+      <ul className="list">
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=39_9qRI-xi0&list=PLxZprgMRn1dLeEGg7TiWmz42Qhf9Xi7zJ">Windows 8.1 And The Trouble In Bliss</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=0zaUXku15mE&list=PLadNJ4UaIf9pXQ_-hv3xMkZlokHAa1pcU">Windows 7 and the trouble in bliss</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=SBV08FfV5DU&pp=ygUSd2luZG93cyB2aXN0YSBkaWVz">Windows Vista Dies</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=pLYrHwLACFM&list=PLvVW-izAXK8948EzhNCbLvhpGR_B9JyYg&index=4&pp=iAQB">Windows 98 Dies</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=JRS-dbwSkf0&list=PLdNrojDkI21FLK271NjBsghpKdOuVIMLd&pp=gAQB">Windows XP And The Chaos in Bliss</a>
+        </li>
+      </ul>
+    </div>
+  </section>
+)
+
 }
 
 export default App

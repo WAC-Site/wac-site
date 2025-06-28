340c89875ad7479afd845345db5ed74b79003929
Horizon7006 <merdo@Merdo-Mac-Pro.local>
2025-06-28 13:21:57 +0300

    fine

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/NavigationBar/NavigationBar'
 import Home from './Home'
-import SecretPage from './secretpage'
+
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
       <div style={{ padding: '1rem' }}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/secretpage" element={<SecretPage />} />
         </Routes>
       </div>
     </Router>

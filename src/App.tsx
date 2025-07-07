17c6c7bb975d263a96c6ef441f891b0a04979ae4
Horizon <mgthedasher@gmail.com>
2025-07-07 20:21:48 +0300

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Home'
+import './App.css';
 
-
-export default function App() {
-  return (
-    <Router>
-      <div style={{ padding: '1rem' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+export default function Home() {
+    return (
+         <section className="section">
+      <div className="container">
+        <div className="images">
+          <figure className="image is-128x128 mb-5">
+          <img src="https://cdn.discordapp.com/attachments/1353412285993062460/1388144110585516143/sticky.png.png?ex=685fe972&is=685e97f2&hm=a7d47ce716275a853979a35ba4e4d1c53acfe42b06c57984567386537d8dc259&" alt="site-image-1" />
+        </figure>
+        <figure className="image is-128x128 mb-5">
+          <img src="https://media.discordapp.net/attachments/1353412285993062460/1388144110883438743/chess.png?ex=685fe972&is=685e97f2&hm=4391d39ec51772fdc339dfd3240d323647c3748494b8d945519ae2cd6f267f31&=" alt="site-image-2" />
+        </figure>
+        <figure className="image is-128x128 mb-5">
+          <img src="https://media.discordapp.net/attachments/1353412285993062460/1388144111152005270/popcorn2.png?ex=685fe972&is=685e97f2&hm=b327eec242699aa6608e451208bb035d7075c12f1467a5bf7ac4dbc7732572f5&=" alt="site-image-3" />
+        </figure>
+        </div>
+        <h1 className="title mb-4">Welcome to the WAC Site!</h1>
+        <p className="subtitle mb-5">This is the WAC site for Windows Animations!</p>
+      <h2 className="title p-4">What is WAC?</h2>
+      <p className="subtitle mb-4">WAC stands for Windows Animation Community. It is a community where people make stories, animations about Windows OSes.</p>
+      <h2 className="title mb-4">Series to watch!</h2>
+      <p className="subtitle mb-4">Here are some recommended series to watch! Definitely watch them :)</p>
+      <ul className="list">
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=39_9qRI-xi0&list=PLxZprgMRn1dLeEGg7TiWmz42Qhf9Xi7zJ">Windows 8.1 And The Trouble In Bliss</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=0zaUXku15mE&list=PLadNJ4UaIf9pXQ_-hv3xMkZlokHAa1pcU">Windows 7 and the chaos in the afterlife</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=SBV08FfV5DU&pp=ygUSd2luZG93cyB2aXN0YSBkaWVz">Windows Vista Dies</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=pLYrHwLACFM&list=PLvVW-izAXK8948EzhNCbLvhpGR_B9JyYg&index=4&pp=iAQB">Windows 98 Dies</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=JRS-dbwSkf0&list=PLdNrojDkI21FLK271NjBsghpKdOuVIMLd&pp=gAQB">Windows XP And The Chaos in Bliss</a>
+        </li>
+        <li className="list-item">
+          <a href="https://www.youtube.com/watch?v=oiglhaQ1dtg&list=PLrbJqOKaKI1CMFvfvBU-66y31Pp_QVoRV">Windows: Search and Death</a>
+        </li>
+        <li className="list-item mb-5">
+          <a href="https://www.youtube.com/watch?v=oLmAzrYJbis&list=PLSzEmMnjb_gonK5vv4tg841pCnuZy16Cq">Windows: The Infection</a>
+      </li>
+        <p className="subtitle">Credits to the all creators of the series :)</p>
+      </ul>
+    </div>
+  </section>
+    )
 }
+
